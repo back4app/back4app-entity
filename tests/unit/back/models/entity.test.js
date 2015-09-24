4f449bfafd987a1bab5ad1b8dc5a85031d17b503
Antônio Davi Macêdo Coêlho de Castro <adavimacedo@gmail.com>
2015-09-24 12:51:56 -0300

    Tests of Entity#type

--- tests/unit/back/models/entity.test.js
+++ tests/unit/back/models/entity.test.js
@@ -4,19 +4,48 @@ var expect = require('chai').expect;
 var Entity = require('../../../../src/back/models/Entity');
 
 describe('Entity', function () {
+  var entity;
+
+  var C1;
+  var C11;
+  var C2;
+
+  var c1;
+  var c11;
+  var c2;
+
+  it('expect to instantiate new Entity without error', function () {
+    entity = new Entity();
+  });
+
   describe('.specify()', function () {
     it('expect to exist as a static method', function () {
       expect(Entity).itself.to.respondTo('specify');
     });
 
-    var C1;
-    var C11;
-    var C2;
-
     it('expect to run without error', function () {
       C1 = require('./C1');
       C11 = require('./C11');
       C2 = require('./C2');
+
+      c1 = new C1();
+      c11 = new C11();
+      c2 = new C2();
+    });
+  });
+
+  describe('#type', function () {
+    it(
+      'expect to exist as an inner method and return the right type',
+      function () {
+        expect(entity).to.have.property('type')
+          .that.equals(Entity);
+        expect(c1).to.have.property('type')
+          .that.equals(C1);
+        expect(c11).to.have.property('type')
+          .that.equals(C11);
+        expect(c2).to.have.property('type')
+          .that.equals(C2);
     });
   });
 });
