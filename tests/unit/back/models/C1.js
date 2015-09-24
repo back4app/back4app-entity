b6efe46219f3485e333b39ff25c8bf9853379abe
Antônio Davi Macêdo Coêlho de Castro <adavimacedo@gmail.com>
2015-09-24 10:46:54 -0300

    C11 done

--- tests/unit/back/models/C1.js
+++ tests/unit/back/models/C1.js
@@ -5,35 +5,52 @@ module.exports = require('../../../../').specify({
     c1A1: {
       type: 'Boolean',
       multiplicity: '1',
-      default: false
+      default: 'false'
     },
     c1A2: {
       type: 'Datetime',
       multiplicity: '0..1',
-      default: new Date()
+      default: 'new Date()'
     },
     c1A3: {
       type: 'Integer',
       multiplicity: '1..*',
-      default: []
+      default: '[0]'
     },
     c1A4: {
       type: 'Object',
       multiplicity: '*',
-      default: null
+      default: 'null'
     },
     c1A5: {
       type: 'Real',
-      multiplicity: '1'
+      multiplicity: '1',
+      default: '0.0'
     },
     c1A6: {
       type: 'String',
+      multiplicity: '0..1',
       default: ''
     },
     c1A7: {
-      type: 'C2'
+      type: 'C1',
+      multiplicity: '1',
+      default: 'new C1()'
     },
-    c1A8: {}
+    c1A8: {
+      type: 'C11',
+      multiplicity: '0..1',
+      default: 'null'
+    },
+    c1A9: {
+      type: 'C2',
+      multiplicity: '1..*',
+      default: '[new C2()]'
+    },
+    c1A10: {
+      type: 'C2',
+      multiplicity: '*'
+    }
   },
   methods: {
     c1M1: c1M1,
@@ -42,11 +59,9 @@ module.exports = require('../../../../').specify({
 });
 
 function c1M1(c1M1P1, c1M1P2) {
-  console.log(c1M1P1);
-  console.log(c1M1P2);
+  return c1M1P1 + c1M1P2;
 }
 
 function c1M2(c1M2P1, c1M2P2) {
-  console.log(c1M2P1);
-  console.log(c1M2P2);
+  return c1M2P1 + c1M2P2;
 }
--- tests/unit/back/models/C11.js
+++ tests/unit/back/models/C11.js
@@ -3,3 +3,35 @@
 //
 
 'use strict';
+
+module.exports = require('C1').specify({
+  attributes: {
+    c11A1: {
+      type: 'Object',
+      multiplicity: '1'
+    }
+  },
+  methods: {
+    c1M1: c1M1,
+    c11M: c11M
+  }
+});
+
+function c1M1(c1M1P1, c1M1P2, c11M1P) {
+  return this.general.c1M1(c1M1P1, c1M1P2) + c11M1P;
+
+}
+
+function c11M1(c1M2P1, c1M2P2) {
+  var toReturn = '';
+
+  for (var attribute in this.general.type.attributes) {
+    toReturn += this[attribute];
+  }
+
+  for (var attribute in this.type.attributes) {
+    toReturn += this[attribute];
+  }
+
+  return toReturn;
+}
