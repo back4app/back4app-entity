14b2d2ee67b6f7e192ae8eff484ff5555829e512
Antônio Davi Macêdo Coêlho de Castro <adavimacedo@gmail.com>
2015-09-24 11:25:57 -0300

    specify test to run without error

--- tests/unit/back/models/entity.test.js
+++ tests/unit/back/models/entity.test.js
@@ -2,14 +2,21 @@
 
 var expect = require('chai').expect;
 var Entity = require('../../../../src/back/models/Entity');
-var C1 = require('./C1');
-var C11 = require('./C11');
-var C2 = require('./C2');
 
 describe('Entity', function () {
   describe('.specify()', function () {
     it('expect to exist as a static method', function () {
       expect(Entity).itself.to.respondTo('specify');
     });
+
+    var C1;
+    var C11;
+    var C2;
+
+    it('expect to run without error', function () {
+      C1 = require('./C1');
+      C11 = require('./C11');
+      C2 = require('./C2');
+    })
   });
 });
