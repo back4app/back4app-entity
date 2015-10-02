80c239d7c856e0e60b3a32ad6887717fb25c4f4c
Antônio Davi Macêdo Coêlho de Castro <adavimacedo@gmail.com>
2015-10-02 13:42:55 -0300

    Improvind MethodCollection.add tests

--- tests/unit/back/models/methods.test.js
+++ tests/unit/back/models/methods.test.js
@@ -54,5 +54,56 @@ describe('methods', function () {
         });
       }).to.throw(AssertionError);
     });
+
+    describe('#add', function () {
+      it('expect to exist as an instance method', function () {
+        expect(methodCollection).to.respondTo('add');
+      });
+
+      it('expect to work with right arguments', function () {
+        methodCollection.add(function () { return 'method3'; }, 'method3');
+      });
+
+      it('expect to not work with wrong arguments', function () {
+        expect(function () {
+          methodCollection.add();
+        }).to.throw(AssertionError);
+
+        expect(function () {
+          methodCollection.add(
+            function () { return 'method4'; },
+            'method4',
+            null
+          );
+        }).to.throw(AssertionError);
+
+        expect(function () {
+          methodCollection.add({}, 'method4');
+        }).to.throw(AssertionError);
+
+        expect(function () {
+          methodCollection.add(function () { return 'method4'; }, {});
+        }).to.throw(AssertionError);
+
+        expect(methodCollection).not.to.respondTo('method4');
+      });
+
+      it('expect to not work with duplicates', function () {
+        expect(function () {
+          methodCollection.add(
+            function () { return 'will never execute'; },
+            'method3'
+          );
+        }).to.throw(AssertionError);
+
+        expect(methodCollection.method3()).to.equal('method3');
+      });
+
+      it('expect to allow execute functions correctly', function () {
+        expect(methodCollection.method1()).to.equal('method1');
+        expect(methodCollection.method2()).to.equal('method2');
+        expect(methodCollection.method3()).to.equal('method3');
+      });
+    });
   });
 });
