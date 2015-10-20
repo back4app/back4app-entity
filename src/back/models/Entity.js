e88952838479984c3d3e5619670199e09bc2cc03
Walkirya Heto <you@example.com>
2015-10-20 15:59:40 -0200

    removing .toString() of id generation

--- src/back/models/Entity.js
+++ src/back/models/Entity.js
@@ -84,7 +84,7 @@ function Entity(attributeValues) {
     );
   }
 
-  var _id = uuid.v4().toString();
+  var _id = uuid.v4();
 
   Object.defineProperty(this, '_id', {
     get: function () {
@@ -131,19 +131,6 @@ function Entity(attributeValues) {
     }
   }
 }
-
-//function _generateUUID() {
-//  var d = new Date().getTime();
-//  var uuid;
-//  uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.
-//    replace(/[xy]/g, function (c) {
-//      var r = (d + Math.random() * 16) % 16 | 0;
-//      d = Math.floor(d / 16);
-//      return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
-//    });
-//  return uuid;
-//
-//};
 
 /**
  * This is a read-only property to get the general Entity Class of the current
