d8d86d9392ffb3cd9fcf024858781d9d3cf6870c
Antônio Davi Macêdo Coêlho de Castro <adavimacedo@gmail.com>
2015-11-01 00:15:35 -0200

    Improving EntitySpecififcation tests regarding isAbstract and dataName properties

--- tests/unit/back/models/entitySpecification.test.js
+++ tests/unit/back/models/entitySpecification.test.js
@@ -34,19 +34,27 @@ describe('EntitySpecification', function () {
         entitySpecification = new EntitySpecification({
           name: 'MyEntity',
           attributes: null,
-          methods: null
+          methods: null,
+          isAbstract: null,
+          dataName: null
         });
 
         entitySpecification = new EntitySpecification({
           name: 'MyEntity',
           attributes: {},
-          methods: {}
+          methods: {},
+          isAbstract: false,
+          dataName: {}
         });
 
         entitySpecification = new EntitySpecification({
           name: 'MyEntity',
           attributes: new attributes.AttributeDictionary(),
-          methods: new methods.MethodDictionary()
+          methods: new methods.MethodDictionary(),
+          isAbstract: false,
+          dataName: {
+            default: 'MyEntity'
+          }
         });
 
         entitySpecification = new EntitySpecification({
@@ -68,21 +76,36 @@ describe('EntitySpecification', function () {
           methods: new methods.MethodDictionary({
             method1: function () { return 'method1'; },
             method2: function () { return 'method2'; }
-          })
+          }),
+          isAbstract: false,
+          dataName: {
+            default: 'MyEntity'
+          }
         });
       }
     );
 
     it('expect to work with right arguments passing as arguments',
       function () {
-        entitySpecification = new EntitySpecification('MyEntity', null, null);
+        entitySpecification = new EntitySpecification(
+          'MyEntity',
+          null,
+          null,
+          null
+        );
 
-        entitySpecification = new EntitySpecification('MyEntity', {}, {});
+        entitySpecification = new EntitySpecification(
+          'MyEntity',
+          {},
+          {},
+          {}
+        );
 
         entitySpecification = new EntitySpecification(
           'MyEntity',
           new attributes.AttributeDictionary(),
-          new methods.MethodDictionary()
+          new methods.MethodDictionary(),
+          {}
         );
 
         entitySpecification = new EntitySpecification(
@@ -106,7 +129,13 @@ describe('EntitySpecification', function () {
           new methods.MethodDictionary({
             method1: function () { return 'method1'; },
             method2: function () { return 'method2'; }
-          })
+          }),
+          {
+            isAbstract: true,
+            dataName: {
+              default: 'MyEntity'
+            }
+          }
         );
       }
     );
@@ -131,7 +160,7 @@ describe('EntitySpecification', function () {
 
     it('expect to not work with wrong arguments', function () {
       expect(function () {
-        entitySpecification = new EntitySpecification({}, {}, {});
+        entitySpecification = new EntitySpecification({}, {}, {}, {});
       }).to.throw(AssertionError);
 
       expect(function () {
@@ -149,6 +178,8 @@ describe('EntitySpecification', function () {
             method1: function () { return 'method1'; },
             method2: function () { return 'method2'; }
           }),
+          isAbstract: true,
+          dataName: 'MyEntity',
           doesNotExist: null
         });
       }).to.throw(AssertionError);
@@ -196,6 +227,38 @@ describe('EntitySpecification', function () {
           function () {}
         );
       }).to.throw(AssertionError);
+
+      expect(function () {
+        entitySpecification = new EntitySpecification({
+          name: 'MyEntity',
+          attributes: new attributes.AttributeDictionary({
+            attribute1: attributes.Attribute.resolve('attribute1'),
+            attribute2: attributes.Attribute.resolve('attribute2')
+          }),
+          methods: new methods.MethodDictionary({
+            method1: function () { return 'method1'; },
+            method2: function () { return 'method2'; }
+          }),
+          isAbstract: {},
+          dataName: 'MyEntity'
+        });
+      }).to.throw(AssertionError);
+
+      expect(function () {
+        entitySpecification = new EntitySpecification({
+          name: 'MyEntity',
+          attributes: new attributes.AttributeDictionary({
+            attribute1: attributes.Attribute.resolve('attribute1'),
+            attribute2: attributes.Attribute.resolve('attribute2')
+          }),
+          methods: new methods.MethodDictionary({
+            method1: function () { return 'method1'; },
+            method2: function () { return 'method2'; }
+          }),
+          isAbstract: false,
+          dataName: function () {}
+        });
+      }).to.throw(AssertionError);
     });
   });
 
@@ -212,6 +275,14 @@ describe('EntitySpecification', function () {
 
       expect(entitySpecification.methods.method2())
         .to.equal('method2');
+
+      expect(entitySpecification.isAbstract)
+        .to.equal(true);
+
+      expect(entitySpecification.dataName)
+        .to.deep.equal({
+          default: 'MyEntity'
+        });
     });
 
     it('expect to be not extensible', function () {
@@ -236,6 +307,18 @@ describe('EntitySpecification', function () {
       }).to.throw(Error);
 
       expect(entitySpecification).to.have.property('methods');
+
+      expect(function () {
+        delete entitySpecification.isAbstract;
+      }).to.throw(Error);
+
+      expect(entitySpecification).to.have.property('isAbstract');
+
+      expect(function () {
+        delete entitySpecification.dataName;
+      }).to.throw(Error);
+
+      expect(entitySpecification).to.have.property('dataName');
     });
 
     it('expect to not allow to change property', function () {
@@ -252,6 +335,22 @@ describe('EntitySpecification', function () {
 
       expect(entitySpecification.methods.method2())
         .to.equal('method2');
+
+      expect(function () {
+        entitySpecification.isAbstract = false;
+      }).to.throw(Error);
+
+      expect(entitySpecification.isAbstract)
+        .to.equal(true);
+
+      expect(function () {
+        entitySpecification.dataName = 'MyDataName';
+      }).to.throw(Error);
+
+      expect(entitySpecification.dataName)
+        .to.deep.equal({
+          default: 'MyEntity'
+        });
     });
   });
 
