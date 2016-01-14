9fccb5bc77b363e3ab74be4649dec9acbc171307
Seiji Akiyama <seiji.akiyama2.0@gmail.com>
2016-01-14 17:15:06 -0200

    Fixes minor details.

--- tests/unit/back/models/user.test.js
+++ tests/unit/back/models/user.test.js
@@ -11,6 +11,7 @@ var user = new User({
   username: 'test',
   email: 'email@test.com',
   password: '$2a$10$Bd4I9p6xSuphD3gSPk006.fMR5/Dr9LjJD7JhvJcm7hpDaS0wJruq'
+  // the hash refers to 'test'
 });
 
 describe('User', function () {
@@ -23,15 +24,14 @@ describe('User', function () {
 
   it('#authenticate', function () {
     var promises = [
-      user.authenticate('teste').then(function (res) {
+      user.authenticate('wrong_password').then(function (res) {
         expect(res).to.equal(false);
       }),
       user.authenticate('test').then(function (res) {
         expect(res).to.equal(true);
       })
     ];
-    return Promise
-      .all(promises);
+    return Promise.all(promises);
   });
 });
 
